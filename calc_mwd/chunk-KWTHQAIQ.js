9908a9a5ae1e0e96802d0f58cd0d5a5ada43e317
mario <somsos112@gmail.com>
2025-03-29 17:57:41 -0600

    add csv support and help added

---
+++ calc_mwd/chunk-KWTHQAIQ.js
@@ -0,0 +1,18 @@
+import{k as m,p as r,q as s,r as d,s as p,w as c}from"./chunk-H5YYNR2H.js";import"./chunk-WHSHN6AA.js";import{La as a,ia as l,xa as e,ya as t,za as n}from"./chunk-OQDNOSON.js";import"./chunk-C6Q5SG76.js";var g=class o{static \u0275fac=function(i){return new(i||o)};static \u0275cmp=l({type:o,selectors:[["csv-example-dialog"]],decls:26,vars:0,consts:[["mat-dialog-title",""],[1,"mat-typography"],[1,"code-example"],["align","end"],["mat-button","","mat-dialog-close",""]],template:function(i,u){i&1&&(e(0,"h2",0),a(1,"Ayuda"),t(),e(2,"mat-dialog-content",1)(3,"h5"),a(4,"Ejemplo de archivo .csv para introducir"),t(),e(5,"p"),a(6,"La cabecera es opcional y el formateado tambi\xE9n"),t(),e(7,"pre",2),a(8,`soilWeight, tamizDiameter
+         0,             8
+       3.4,             4
+     21.26,             2
+     34.73,             1
+     35.69,           0.5
+     31.31,          0.25
+     38.26,         0.053
+     25.37,             0 `),t(),n(9,"hr"),e(10,"h5"),a(11,"Ejemplo de archivo .csv generado"),t(),e(12,"p"),a(13,`Se genera un archivo de texto plano, de tipo .csv (Coma Separated Version),
+el cual puede ser abierto con programas como Excel.`),t(),e(14,"p"),a(15,"No se genera el formateado, se agrego aqu\xED para comodidad visual."),t(),e(16,"pre",2),a(17,`soilWeight,  tamizDiameter,  tamizDiam,  soilPortions,  MWDs    ,   totalSoil,  MWDTotal
+  0.000000,       8.000000,   6.000000,      0.017893,  0.107357,  190.020000,  0.953859829
+  3.400000,       4.000000,   3.000000,      0.111883,  0.335649,            ,
+ 21.260000,       2.000000,   1.500000,      0.182770,  0.274155,            ,
+ 34.730000,       1.000000,   0.750000,      0.187822,  0.140867,            ,
+ 35.690000,       0.500000,   0.375000,      0.164772,  0.061790,            ,
+ 31.310000,       0.250000,   0.151500,      0.201347,  0.030504,            ,
+ 38.260000,       0.053000,   0.026500,      0.133512,  0.003538,            ,
+ 25.370000,       0.000000,           ,              ,          ,            ,`),t(),n(18,"hr"),e(19,"h5"),a(20,"Cr\xE9ditos"),t(),e(21,"p"),a(22,"Mario Marquez V."),t()(),e(23,"mat-dialog-actions",3)(24,"button",4),a(25,"Cerrar"),t()())},dependencies:[c,r,s,p,d,m],styles:[".code-example[_ngcontent-%COMP%]{background-color:#d3d3d3;overflow-x:scroll}"]})};export{g as CsvExampleDialogComponent};
